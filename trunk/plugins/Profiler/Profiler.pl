654be8dfe2b0804b21dc11db6ce3e17e7dcf9036
Chad Everett <github@jayseae.cxliv.org>
2009-07-17 11:33:38 -0400

    Update license to Artistic - 0.1.1

--- trunk/plugins/Profiler/Profiler.pl
+++ trunk/plugins/Profiler/Profiler.pl
@@ -1,7 +1,17 @@
 # ===========================================================================
-# Copyright 2005, Everitz Consulting (mt@everitz.com)
+# A Movable Type plugin to add a link to the author for editing profiles.
+# Copyright 2005 Everitz Consulting <everitz.com>.
 #
-# Licensed under the Open Software License version 2.1
+# This program is free software:  You may redistribute it and/or modify it
+# it under the terms of the Artistic License version 2 as published by the
+# Open Source Initiative.
+#
+# This program is distributed in the hope that it will be useful but does
+# NOT INCLUDE ANY WARRANTY; Without even the implied warranty of FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the Artistic License with this program.
+# If not, see <http://www.opensource.org/licenses/artistic-license-2.0.php>.
 # ===========================================================================
 package MT::Plugin::Profiler;
 
@@ -12,13 +22,13 @@ use MT;
 
 # version
 use vars qw($VERSION);
-$VERSION = '0.1.0';
+$VERSION = '0.1.1';
 
 my $about = {
   name => 'MT-Profiler',
-  description => 'Adds a link to the author list for editing profiles.',
+  description => 'Add a link to the author for editing profiles.',
   author_name => 'Everitz Consulting',
-  author_link => 'http://www.everitz.com/',
+  author_link => 'http://everitz.com/',
   version => $VERSION,
 };
 my $profiler = MT::Plugin::Profiler->new($about);
